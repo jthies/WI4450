d3ea0c28eb7aabad0d7769de4432e41b6bf90db5
Jonas Thies <16190001+jthies@users.noreply.github.com>
2023-01-29 06:08:50 +0100

    introduce a struct to represent the stencil

--- operations.cpp
+++ operations.cpp
@@ -3,25 +3,27 @@
 
 void init(int n, double* x, double value)
 {
+  // YOUR TASK
+  return;
 }
 
 double dot(int n, double const* x, double const* y)
 {
-  return 0;
+  // YOUR TASK
+  return 0.0;
 }
 
 void axpby(int n, double a, double const* x, double b, double const* y, double* z)
 {
+  // YOUR TASK
   return;
 }
 
-void apply_op_lapl2D(int nx, int ny, double const* u, double* v)
-{
-  return;
-}
-void apply_op_lapl3D(int nx, int ny, int nz,
+//! apply a 7-point stencil to a vector
+void apply_stencil3d(stencil const* S,
         double const* u, double* v)
 {
+  // YOUR TASK
   return;
 }
 
--- operations.hpp
+++ operations.hpp
@@ -1,32 +1,31 @@
 #pragma once
 
+//////////////////////////////////
+// Vector operations            //
+//////////////////////////////////
+
 void init(int n, double* x, double alue);
 
 double dot(int n, double const* x, double const* y);
 
 void axpby(int n, double a, double const* x, double b, double const* y, double* z);
 
-inline int index2d(int nx, int ny, int i, int j)
+//////////////////////////////////
+// Linear operator application  //
+//////////////////////////////////
+
+typedef struct
 {
-  return j*nx + i;
-}
+  int nx, ny, nz;
+  double center, north, east, south, west, bottom, top;
+} stencil3d;
 
 inline int index3d(int nx, int ny, int nz, int i, int j, int k)
 {
   return (k*ny +j)*nx + i;
 }
 
-//! apply the 5-point finite difference stencil
-//! representing the Laplace operator on an nx x ny
-//! grid. In each direction, the grid points are given
-//! by
-//!
-//! x=0                           x=1
-//!  |-----|-----| ... |-----|-----|
-//! i=0   i=1   i=2   i=n-2 i=n-1 i=n
-void apply_op_lapl2D(int nx, int ny,
-        double const* u, double* v);
-
-void apply_op_lapl3D(int nx, int ny, int nz,
+//! apply a 7-point stencil to a vector
+void apply_stencil3d(stencil const* S,
         double const* u, double* v);
 
