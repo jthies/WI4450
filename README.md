4bf1592097c8ad88913fb493c7dbab8eb3638397
Jonas Thies <16190001+jthies@users.noreply.github.com>
2023-01-29 09:17:44 +0100

    update PDE description

--- README.md
+++ README.md
@@ -14,9 +14,8 @@ a standard partial differential equation, the 3D Poisson equation:
 on a unit cube $\Omega = [0\dots 1] \times [0 \dots 1] \times [0\dots 1]`$, subject to Dirichlet boundary conditions
 
 ```math
-u(x,y,z)=0 \mathrm{if} x=0, x=1, y=0, \mathrm{or} y=1
-u(x,y,z)=g(x,y) \mathrm{
-u(x,y,z)=g(y) \mathrm{if} x=0 \mathrm{or} x=1
+u(x,y,z)=0 \mathrm{if} x=0, x=1, y=0, \mathrm{or} y=1 \mathrm{or} z=1
+u(x,y,z)=g(x,y) \mathrm{if} z=0
 ```
 
 ## Your Tasks
--- cg_poisson.cpp
+++ cg_poisson.cpp
@@ -1,6 +1,30 @@
 #include "operations.hpp"
 #include "cg_solver.hpp"
 
+#include <cmath>
+
+// Main program that solves the 3D Poisson equation
+// on a unit cube. The grid size (nx,ny,nz) can be 
+// passed to the executable like this:
+//
+// cg_poisson <nx> <ny> <nz>
+//
+// Boundary conditions and forcing term f(x,y,z) are
+// hard-coded in this file. See README.md for details
+// on the PDE and boundary conditions.
+
+// Forcing term
+double f(double x, double y, double z)
+{
+  return z*sin(2*M_PI*x)*std::sin(M_PI*y) + 8*z*z*z;
+}
+
+// boundary condition at z=0
+double g_0(double x, double y)
+{
+  return x*(1.0-x)*y*(1-y);
+}
+
 int main()
 {
   return 0;
