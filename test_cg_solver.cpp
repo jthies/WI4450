ab6a54d308903ec3ab868698328131d0dbe24f58
Femke Klein <fpmklein@login02.delftblue.tudelft.nl>
2023-03-07 15:01:43 +0100

    Made new file with test function for cg method, now consists of two tests. Both tests perform cg for a symmetric positive definite matrix and test whether the residual norm is small enough.

---
+++ test_cg_solver.cpp
@@ -0,0 +1,75 @@
+#include "gtest_mpi.hpp"
+
+#include "operations.hpp"
+
+#include "cg_solver.hpp"
+
+#include <iostream>
+
+#include <cmath>
+
+#include <limits>
+
+
+TEST(cg_example, identity)
+{
+  const int nx=2, ny=2, nz=2;
+  const int n = nx * ny * nz;
+  double *x = new double[n];
+  double *b = new double[n];
+  double tol = std::sqrt(std::numeric_limits<double>::epsilon());
+  int maxIter = n;
+  double resNorm;
+  int numIter;
+
+  stencil3d S;
+  S.nx = nx; S.ny = ny; S.nz = nz;
+  S.value_c = 1;
+  S.value_n = 0;
+  S.value_e = 0;
+  S.value_s = 0;
+  S.value_w = 0;
+  S.value_b = 0;
+  S.value_t = 0;
+
+  init(n, x, 1.0);
+  init(n, b, 2.0);
+  cg_solver(&S, n, x, b, tol, maxIter, &resNorm, &numIter);
+  EXPECT_NEAR(0, resNorm, tol);
+  std::cout<<"matrix3d is a idenity matrix and b is a vector with all elements equal to 2"<<std::endl;
+  std::cout<<"cg value for x, "<< "true value for x"<<std::endl;
+  for (int i=0; i<n; i++) std::cout<<x[i]<<", 2 "<<std::endl;
+
+  delete [] x;
+  delete [] b;
+}
+
+TEST(cg_example, stencil)
+{
+  const int nx=2, ny=2, nz=2;
+  const int n = nx * ny * nz;
+  double *x = new double[n];
+  double *b = new double[n];
+  double tol = std::sqrt(std::numeric_limits<double>::epsilon());
+  int maxIter = n;
+  double resNorm;
+  int numIter;
+
+  stencil3d S;
+  S.nx = nx; S.ny = ny; S.nz = nz;
+  S.value_c = 8;
+  S.value_n = 2;
+  S.value_e = 4;
+  S.value_s = 2;
+  S.value_w = 4;
+  S.value_b = 1;
+  S.value_t = 1;
+
+  init(n, x, 1.0);
+  init(n, b, 0.0);
+  cg_solver(&S, n, x, b, tol, maxIter, &resNorm, &numIter);
+  EXPECT_NEAR(0, resNorm, tol);
+
+  delete [] x;
+  delete [] b;
+}
