0506953ce67022acaf16bd267a4f5d72ceac8507
Zhaonan Meng <zhaonanmeng@login04.delftblue.tudelft.nl>
2023-03-07 11:15:19 +0100

    update init, dot, axpby, apply_stencil

--- operations.cpp
+++ operations.cpp
@@ -3,19 +3,26 @@
 
 void init(int n, double* x, double value)
 {
-  // [...]
+  #pragma omp parallel for 
+  for (int i=0; i<n; i++)
+     x[i] = value;
   return;
 }
 
 double dot(int n, double const* x, double const* y)
 {
-  // [...]
-  return 0.0;
+  double res;
+  #pragma omp parallel for reduction(+:res)
+  for (int i=0; i<n; i++)
+     res += x[i]*y[i];
+  return res;
 }
 
 void axpby(int n, double a, double const* x, double b, double* y)
 {
-  // [...]
+  #pragma omp parallel for
+  for (int i=0; i<n; i++)
+     y[i] = (a * x[i] + b * y[i]);
   return;
 }
 
@@ -23,7 +30,50 @@ void axpby(int n, double a, double const* x, double b, double* y)
 void apply_stencil3d(stencil3d const* S,
         double const* u, double* v)
 {
-  // [...]
+  int nx=S->nx, ny=S->ny, nz=S->nz;
+  double ele; // value of v[S->index_c(i,j,k)]
+  
+  #pragma omp parallel for reduction(+:ele)
+  for (int k=0; k<nz; k++) 
+     for (int j=0; j<ny; j++)
+        for(int i=0; i<nx; i++)
+ // for (int i=0; i<nx; i++)
+ //   for (int j=0; j<ny; j++)
+ //     for(int k=0; k<nz; k++) 
+	{
+	   ele  = S->value_c * u[S->index_c(i,j,k)];
+	
+           if (i==0||i==nx-1) 
+	      ele += (i==0) ? 
+	         (u[S->index_e(i,j,k)] * S->value_e): 
+		 (u[S->index_w(i,j,k)] * S->value_w);
+	   else 
+	      ele += u[S->index_e(i,j,k)]*S->value_e + 
+			    u[S->index_w(i,j,k)]*S->value_w; 	
+
+           if (j==0||j==ny-1) 
+	      ele += (j==0) ? 
+	         (u[S->index_n(i,j,k)] * S->value_n): 
+		 (u[S->index_s(i,j,k)] * S->value_s);
+	   else 
+	      ele += u[S->index_n(i,j,k)]*S->value_n + 
+			    u[S->index_s(i,j,k)]*S->value_s; 	
+
+	   /*I think index_b and index_t defined in operations.hpp is incorrect
+	    * b and t should be reversed
+	    * discuss this with Jonas!
+	    * */ 
+           if (k==0||k==nz-1) 
+	      ele += (k==0) ? 
+	         (u[S->index_b(i,j,k)] * S->value_b): 
+		 (u[S->index_t(i,j,k)] * S->value_t);  
+	   else 
+	      ele += u[S->index_t(i,j,k)]*S->value_t + 
+		            u[S->index_b(i,j,k)]*S->value_b; 	
+
+	   v[S->index_c(i,j,k)] = ele;
+	}
+ 
   return;
 }
 
