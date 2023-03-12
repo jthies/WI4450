51a918390623d962fcc9d878ee08ab10a887e5f5
Femke Klein <fpmklein@login03.delftblue.tudelft.nl>
2023-03-12 17:28:20 +0100

    changed max number iterations

--- main_cg_poisson.cpp
+++ main_cg_poisson.cpp
@@ -102,7 +102,7 @@ int main(int argc, char* argv[])
       }
 
     // solve the linear system of equations using CG
-    int numIter, maxIter=500;
+    int numIter, maxIter=1000;
     double resNorm, tol=std::sqrt(std::numeric_limits<double>::epsilon());
 
     try
