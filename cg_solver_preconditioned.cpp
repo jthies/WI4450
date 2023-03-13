6d70c6cfdb4f2ead668bc3d04c240a5717abc3ea
Femke Klein <fpmklein@login02.delftblue.tudelft.nl>
2023-03-13 01:01:35 +0100

    old commented code removed

--- cg_solver_preconditioned.cpp
+++ cg_solver_preconditioned.cpp
@@ -17,20 +17,10 @@ void precond_cg_solver(stencil3d const* op, int n, double* x, double const* b,
   {
     throw std::runtime_error("mismatch between stencil and vector dimension passed to cg_solver");
   }
-  //double trace = n * op->value_c;
-  /*stencil3d invM;
-  invM.nx = op->nx; invM.ny = op->ny; invM.nz = op->nz;
-  invM.value_c = 1.0/(op->value_c);//(op->value_c)/trace;//(op->value_c)/trace;
-  invM.value_n = 0.0;
-  invM.value_e = 0.0;
-  invM.value_s = 0.0;//-(op->value_s)/(op->value_c)*(op->value_s)/trace; //-(op->value_s)/trace;
-  invM.value_w = 0.0;//-(op->value_w)/(op->value_c)*(op->value_w)/trace; //-(op->value_w)/trace;
-  invM.value_t = 0.0;
-  invM.value_b = 0.0;//-(op->value_b)/(op->value_c)*(op->value_b)/trace; //-(op->value_b)/trace;
-  */
   double *p = new double[n];
   double *q = new double[n];
   double *r = new double[n];
+  //extra vector for preconditioning
   double *z = new double[n];
 
   double alpha, beta, rho=1.0, rho_old=0.0;
@@ -44,7 +34,7 @@ void precond_cg_solver(stencil3d const* op, int n, double* x, double const* b,
   axpby(n, 1.0, b, -1.0, r);
 
   init(n, p, 0.0);
-  //init(n, q, 0.0);
+  init(n, q, 0.0);
 
   // start CG iteration
   int iter = -1;
